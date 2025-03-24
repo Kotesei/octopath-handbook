6815968ed4a32e3742f3049f8e5669b3e01460b4
Kotesei <192398548+Kotesei@users.noreply.github.com>
2025-03-24 17:09:40 -0400

    added a few functions to filter buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import TravelerList from "./components/TravelerList";
 import { travelers } from "./helpers/FilterCharacters";
@@ -10,24 +10,60 @@ export default function App() {
     gender: "",
     types: [],
     influence: "",
-    minrank: "",
-    maxrank: "",
+    rank: "",
   });
+  const [filteredTravelers, setFilteredTravelers] = useState(
+    travelers.filteredTravelers
+  );
 
   function applyFilters(value) {
-    console.log(value);
+    setFilters((prev) => {
+      if (travelers.jobs.includes(value)) return { ...prev, job: value };
+      if (travelers.genders.includes(value)) return { ...prev, gender: value };
+      if (travelers.influences.includes(value))
+        return { ...prev, influence: value };
+      if (travelers.ranks.includes(value)) return { ...prev, rank: value };
+
+      if (travelers.types.includes(value)) {
+        return {
+          ...prev,
+          types: prev.types.includes(value)
+            ? prev.types.filter((t) => t !== value)
+            : [...prev.types, value],
+        };
+      }
+
+      return prev;
+    });
   }
-  console.log(travelers);
+
+  useEffect(() => {
+    travelers.resetFilters();
+
+    if (filters.job) travelers.filterByJob(filters.job);
+    if (filters.gender) travelers.filterByGenders(filters.gender);
+    if (filters.influence) travelers.filterByInfluence(filters.influence);
+    if (filters.rank) travelers.filterByRank(filters.rank);
+    if (filters.types.length > 0) travelers.filterByType(...filters.types);
+
+    setFilteredTravelers([...travelers.filteredTravelers]);
+  }, [filters]);
 
   return (
     <div className="bg-black h-full w-full flex items-center gap-5 overflow-hidden">
       <TravelerList travelers={travelers.unsortedTravelers} />
-      <TravelerList travelers={travelers.filteredTravelers} filtered />
+      <TravelerList travelers={filteredTravelers} filtered />
       <div className="flex flex-col gap-10 w-200">
         <Filter triggerFilters={applyFilters} filter={travelers.jobs} />
         <Filter triggerFilters={applyFilters} filter={travelers.genders} />
         <Filter triggerFilters={applyFilters} filter={travelers.types} />
+
         <Filter triggerFilters={applyFilters} filter={travelers.influences} />
+
+        <Filter
+          triggerFilters={applyFilters}
+          filter={travelers.ranks.sort((a, b) => a.length - b.length)}
+        />
       </div>
     </div>
   );
